8db081b05f97ac30302b30ec4a2b4c208c8f2dd5
Thom Porter <thomporter@gmail.com>
2024-04-08 22:37:52 +0000

    setup withastro/action for GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gh.rpgmobob.com',
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   }),]
